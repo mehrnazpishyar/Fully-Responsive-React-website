cf57a7cc76675545cd7d00ad9185b60caa392f64
mehrnazpishyar <pishyar.mehrnaz@gmail.com>
2022-05-02 18:12:47 +0430

    create navbar & header & feature components

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export { default as Blog } from './blog/Blog.jsx';
+export { default as Features } from './feature/Feature.jsx';
+export { default as Footer } from './footer/Footer.jsx';
+export { default as Header } from './header/Header.jsx';
+export { default as Possibility } from './possibility/Posibility.jsx';
+export { default as WhatGPT3 } from './whatGPT3/WhatGPT3.jsx';
